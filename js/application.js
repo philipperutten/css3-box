49c3f2041102b64a62442d869fd63a8b23207b6e
Philippe <philippe.rutten@gmail.com>
2014-05-21 19:53:20 +0200

    first commit

---
+++ js/application.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+/*	//	Add headings to sections and build nav
+		$('section').each(function(cb){
+
+			if($(this).attr('data-title')==undefined)return;
+			
+			// get title
+			var title = $(this).attr('data-title');
+
+			// make id
+			var id = title.replace(/\s+/g, '-');
+
+			// add id to sections
+//			$(this).attr('id',id);
+
+			// add heading
+			var h = document.createElement('h2');
+			$(h).html(title)
+				.css({'fontSize':'130%','text-transform':'uppercase'})
+				.prependTo(this);
+
+			//add nav item
+//			var li = document.createElement('li');
+//			$(li).html('<a href="#'+id+'">'+title+'</a>').appendTo('#side-nav');
+		});
+*/		
+//		$('#content').scrollspy();
+});
