fba052c3d1a6d0ecd8aee633470ec86085c85c08
Philippe <philippe.rutten@gmail.com>
2014-09-16 17:52:48 +0200

    refactor hidden menu script
    - hide menu on click anywhere
    - refactor toggle code to reevaluate every time whether menu is showing

--- demo/js/main.js
+++ demo/js/main.js
@@ -4,22 +4,29 @@ $(document).ready(function() {
 	var $navLayer 	= $('#nav-layer');
 	var $navToggle 	= $('#menu-toggle');
 
-	// activate menu toggle
+	var menuShowing = function(){
+		return ($mainLayer.css('left') !='0px') ? true : false;
+	}
+	var hideMenu = function(){
+		$mainLayer
+			.css('left','');
+		;
+		return;
+	}
+	var showMenu = function(){
+		var menuWidth=$navLayer.outerWidth();
+		$mainLayer
+			.css('left',menuWidth)
+		;
+		return;
+	}
+
+	// show/hide menu with toggle button
 	$navToggle.click(function(){
-		var menuShown = ($mainLayer.css('left') !='0px') ? true : false;
-		var hideMenu = function(){
-			$mainLayer
-				.css('left','');
-			;
-			return;
-		}
-		var showMenu = function(){
-			var menuWidth=$navLayer.outerWidth();
-			$mainLayer
-				.css('left',menuWidth)
-			;
-			return;
-		}
-		if(menuShown){ hideMenu() }else{ showMenu() };
+		if(menuShowing()){ hideMenu() }else{ showMenu() };
+	});
+	// hide menu when clicking anywhere
+	$(document).click(function(){
+		if(menuShowing()){ hideMenu() };
 	});
 });
